854ed55c0d32118b2ce3523264cf31f6e61b9a24
ChampKP <kitphon@odds.team>
2024-12-12 14:01:55 +0700

    refactoring by changing the return type and reducing the number of error-handling lines of code in cart handlers

--- src/handlers/cart.rs
+++ src/handlers/cart.rs
@@ -1,25 +1,27 @@
-use actix_web::{delete, post, web, HttpResponse};
-use sea_orm::{ActiveModelTrait, DatabaseConnection, EntityTrait, QueryFilter, ColumnTrait};
+use actix_web::{delete, post, web, HttpResponse, Responder};
+use sea_orm::{ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 use serde::Deserialize;
 use validator::Validate;
 
 use crate::api_error::APIError;
-use crate::auth_utils::Claims; 
+use crate::auth_utils::Claims;
 use entity::carts;
 
 #[derive(Deserialize, Validate)]
 struct CartRequest {
     pub product_id: i64,
-    pub amount: i32
+    pub amount: i32,
 }
 
 #[post("/carts/add")]
 pub async fn add(
-    db: web::Data<DatabaseConnection>, req: web::Json<CartRequest>, claims: web::ReqData<Claims>
+    db: web::Data<DatabaseConnection>,
+    req: web::Json<CartRequest>,
+    claims: web::ReqData<Claims>,
 ) -> Result<HttpResponse, APIError> {
     match req.validate() {
         Ok(_) => (),
-        Err(e) => return Err(APIError::ValidationError(e. to_string())),
+        Err(e) => return Err(APIError::ValidationError(e.to_string())),
     };
 
     let customer_id: i64 = claims.sub.into();
@@ -51,50 +53,44 @@ pub async fn add(
     };
 
     match new_item.insert(db.get_ref()).await {
-        Ok(_) => Ok(
-            HttpResponse::Created().finish()
-        ),
-        Err(e) => Err(APIError::DatabaseError(e))
+        Ok(_) => Ok(HttpResponse::Created().finish()),
+        Err(e) => Err(APIError::DatabaseError(e)),
     }
 }
 
 #[delete("/carts/clear")]
-async fn clear(db: web::Data<DatabaseConnection>, claims: web::ReqData<Claims>) -> Result<HttpResponse, APIError> {
+async fn clear(
+    db: web::Data<DatabaseConnection>,
+    claims: web::ReqData<Claims>,
+) -> Result<impl Responder, APIError> {
     let customer_id: i64 = claims.sub.into();
 
-    match carts::Entity::delete_many()
+    carts::Entity::delete_many()
         .filter(carts::Column::CustomerId.eq(customer_id))
         .exec(db.get_ref())
-        .await
-    {
-        Ok(_) => Ok(HttpResponse::Ok().json({
-            serde_json::json!({ "message": "Cart cleared successfully" })
-        })),
-        Err(e) => Err(APIError::DatabaseError(e))
-    }
+        .await?;
+
+    Ok(web::Json(serde_json::json!({ "message": "Cart cleared successfully" })))
 }
 
 #[delete("/carts/{product_id}")]
 async fn remove_product(
-    db: web::Data<DatabaseConnection>, product_id_path: web::Path<i32>, claims: web::ReqData<Claims>
-) -> Result<HttpResponse, APIError> {
+    db: web::Data<DatabaseConnection>,
+    product_id_path: web::Path<i32>,
+    claims: web::ReqData<Claims>,
+) -> Result<impl Responder, APIError> {
     let product_id = product_id_path.into_inner();
     if product_id <= 0 {
-        return Err(
-            APIError::ValidationError("Invalid Product ID.".to_owned())
-        )
+        return Err(APIError::ValidationError("Invalid Product ID.".to_owned()));
     }
+
     let customer_id: i64 = claims.sub.into();
 
-    match carts::Entity::delete_many()
+    carts::Entity::delete_many()
         .filter(carts::Column::ProductId.eq(product_id))
         .filter(carts::Column::CustomerId.eq(customer_id))
         .exec(db.get_ref())
-        .await
-    {
-        Ok(_) => Ok(HttpResponse::Ok().json({
-            serde_json::json!({ "message": "The product was removed from the cart successfully." })
-        })),
-        Err(e) => Err(APIError::DatabaseError(e))
-    }
+        .await?;
+
+    Ok(web::Json(serde_json::json!({ "message": "The product was removed from the cart successfully." })))
 }
